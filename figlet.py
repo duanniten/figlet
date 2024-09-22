73dddabcc777a2dccae72f32ce1c96b016d6f7f1
duanniten <duann_@hotmail.com>
2024-09-22 11:36:03 -0300

    implemetation get_text

--- figlet.py
+++ figlet.py
@@ -1,4 +1,5 @@
 import sys
+import pyfiglet
 
 if len(sys.argv) == 3:
     if sys.argv[1] != '-f' or sys.argv[1] != '--font':
@@ -6,3 +7,25 @@ if len(sys.argv) == 3:
     FONT = sys.argv[2]
 elif len(sys.argv) != 1: 
     sys.exit("too many or too few argv")
+    
+
+def main():
+    # Get imput text
+    text = get_text()
+
+    # Get text to print
+    text = fliglet_text(text)
+
+    # print text
+    print_text(text)
+
+def get_text():
+    try: 
+        return input("Input: ")
+    except EOFError:
+        sys.exit("Should enter a text")
+
+
+
+if __name__ == '__main__':
+    main()
